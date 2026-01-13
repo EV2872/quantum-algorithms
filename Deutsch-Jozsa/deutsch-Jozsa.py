e8cf71fee67bbc6abff4737a95a7a232da2271d0
evian <alu0101395548@ull.edu.es>
2026-01-13 23:06:21 +0000

    deutsch-jozsa working

--- Deutsch-Jozsa/deutsch-Jozsa.py
+++ Deutsch-Jozsa/deutsch-Jozsa.py
@@ -13,86 +13,83 @@ def create_Command_Line_Parser() -> argparse.ArgumentParser:
     )
     # Argumento posicional obligatorio
     parser.add_argument(
-        "n_cubits",
+        "n_qubits",
         type=int,
-        help="Number of cubits used in the circuit, must be bigger than 0 and an integer"
+        help="Number of qubits used in the circuit, must be bigger than 0 and an integer"
     )
     return parser
 
-def create_oracle(n_cubits: int) -> QuantumCircuit:
-    circuit: QuantumCircuit = QuantumCircuit(n_cubits + 1)
+def create_oracle(n_qubits: int) -> QuantumCircuit:
+    circuit: QuantumCircuit = QuantumCircuit(n_qubits + 1)
 
     if np.random.randint(0, 2):
-        circuit.x(n_cubits) # Aplica al bit auxiliar
+        circuit.x(n_qubits)
     if np.random.randint(0, 2):
         return circuit
-    
+
     # Si es balanceada
     on_states = np.random.choice(
-        range(2**n_cubits),  # numbers to sample from
-        2**n_cubits // 2,  # number of samples
-        replace=False,  # makes sure states are only sampled once
+        range(2**n_qubits),
+        2**n_qubits // 2, # nos quedamos con la mitad
+        replace=False,
     )
-
+    # f(x) = 1 solo para x ∈ {on_states}
     for state in on_states:
-        for qubit, bit in enumerate(reversed(bin(state)[2:])):
+        little_endian_format: str = list(reversed(bin(state)[2:].zfill(n_qubits)))
+        for qubit, bit in enumerate(little_endian_format):
             if bit == "1":
                 circuit.x(qubit)
-        circuit.mcx(list(range(n_cubits)), n_cubits)
-        for qubit, bit in enumerate(reversed(bin(state)[2:])):
+        circuit.mcx(list(range(n_qubits)), n_qubits)
+        for qubit, bit in enumerate(little_endian_format):
             if bit == "1":
                 circuit.x(qubit)
-    
+
     return circuit
 
 def run_circuit(circuit: QuantumCircuit) -> Any:
     simulator: AerSimulator = AerSimulator()
     compiled_circuit: QuantumCircuit = transpile(circuit, simulator)
-    # Ejecutar, La distribución de todos los shots representa 
-    # las probabilidades de cada estado, shot es una ejecucioón
     job: AerJob = simulator.run(compiled_circuit, shots=1)
     return job.result()
 
-def is_constant(result: Any) -> bool: # true constant
+def is_constant(result: Any) -> bool: # true si es constante
     bits_string: str = list(result.get_counts().keys())[0]
     if '1' in bits_string:
         return False
     return True
 
 def main() -> int:
-    parser: argparse.ArgumentParser = create_Command_Line_Parser()
-    arguments: argparse.Namespace = parser.parse_args()
-    n_cubits: int = arguments.n_cubits # 0...n-1
-    # Creamos el circuito con n + 1 y n bits clasicos para guardar la información 
-    qc: QuantumCircuit = QuantumCircuit(n_cubits + 1, n_cubits) # Todos los cubits se inicializan a |0⟩
+    n_qubits: int = int(input('Introduce the number of qubits: ')) # 0...n-1
+    print('\n')
+    qc: QuantumCircuit = QuantumCircuit(n_qubits + 1, n_qubits) # Todos los cubits se inicializan a |0⟩ por defecto
 
     # Aplicamos la puerta NOT a el bit auxiliar (n) y convertirlo a |1⟩
-    qc.x(n_cubits)
+    qc.x(n_qubits)
     qc.barrier()
 
-    # Aplicamos superposición a todos los cubits, el auxiliar pasa a ser |-⟩ para aprovechar 
+    # Aplicamos superposición a todos los cubits, el auxiliar pasa a ser |-⟩ para aprovechar
     # el paralelismo con la devolución de fase
-    qc.h(range(n_cubits + 1))
+    qc.h(range(n_qubits + 1))
     qc.barrier()
 
     # Consultamos al oraculo Uf​∣x⟩∣y⟩=∣x⟩∣y⊕f(x)⟩ ----> C𝑋 |𝑐 𝑡⟩ = |𝑐 𝑡 ⊕ 𝑐⟩ | f(x)=0 | f(x)=1
-    qc.compose(create_oracle(n_cubits), inplace=True)
+    qc.compose(create_oracle(n_qubits), inplace=True)
     qc.barrier()
-    
+
     # Aplicamos superposición de nuevo
-    qc.h(range(n_cubits))
+    qc.h(range(n_qubits))
     qc.barrier()
 
     # Medimos los cubits salvo el ultimo que no hace falta
-    qc.measure(range(n_cubits), range(n_cubits))
-    
+    qc.measure(range(n_qubits), range(n_qubits))
+
     print(qc.draw('text'))
 
     # Obtener resultados
     result = run_circuit(qc)
     counts = result.get_counts()
 
-    print("Resultados de la medición:")
+    print("Result of the measure:")
     print(counts)
 
     if is_constant(result):
